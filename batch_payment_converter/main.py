7965fab673eae383348eb6a87cf3e82897789c3b
Jonathan Rainer <jonrainer@googlemail.com>
2018-11-25 07:48:34 +0000

    Refactored the project structure

---
+++ batch_payment_converter/main.py
@@ -0,0 +1,8 @@
+import wx
+
+from batch_payment_converter.gui.main_window import MainWindow
+
+if __name__ == "__main__":
+    app = wx.App(False)
+    system = MainWindow(None, "Batch Batch Payment Converter")
+    app.MainLoop()
--- payment_processor/main.py
+++
@@ -1,8 +0,0 @@
-import wx
-
-from payment_processor.interface import MainWindow
-
-if __name__ == "__main__":
-    app = wx.App(False)
-    system = MainWindow(None, "Batch Payment Processor")
-    app.MainLoop()
