06357fa227dc725e97d677e38b59ca781ed25a17
Jonathan Rainer <jonrainer@googlemail.com>
2019-02-17 21:07:27 +0000

    Rationalised Test File into more classes

--- tests/integration_tests/integration_test_converter.py
+++ tests/integration_tests/integration_test_converter.py
@@ -11,15 +11,10 @@ from batch_payment_converter.converter.processed_payments import *
 from batch_payment_converter.converter.formats import XeroFormat
 
 
-class ConvertFileNoFrontend(unittest.TestCase):
+class ConverterTestUtils(object):
 
-    def setUp(self):
+    def __init__(self):
         self.converter = Converter()
-
-    def tearDown(self):
-        files = glob.glob("{0}{1}{2}".format(str(Path("..", "..", "output").absolute()), os.sep, "test_output*"))
-        for f in files:
-            os.remove(f)
 
     def create_file(self, format, output_file_name):
         processed_payments = self.converter.convert_file(
@@ -28,38 +23,32 @@ class ConvertFileNoFrontend(unittest.TestCase):
         output_path = Path("..", "..", "output", output_file_name)
         return self.converter.write_output_file(output_path, processed_payments)
 
-    def test_convert_file_no_frontend_natwest(self):
-        output_path = self.create_file(NatwestBankLinePayment, "test_output_natwest")
-        self.assertTrue(os.path.isfile(str(output_path)))
 
-    def test_convert_file_no_frontend_barclays(self):
-        output_path = self.create_file(BarclaysPayment, "test_output_barclays")
+class ConvertNatwestFileNoFrontend(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.utils = ConverterTestUtils()
+
+    def tearDown(self):
+        files = glob.glob("{0}{1}{2}".format(str(Path("..", "..", "output").absolute()), os.sep, "test_output*"))
+        for f in files:
+            os.remove(f)
+
+    def test_convert_file_no_frontend_natwest(self):
+        output_path = self.utils.create_file(NatwestBankLinePayment, "test_output_natwest")
         self.assertTrue(os.path.isfile(str(output_path)))
 
     def test_natwest_dates_format_correct(self):
-        output_path = self.create_file(NatwestBankLinePayment, "test_output_natwest_dates")
+        output_path = self.utils.create_file(NatwestBankLinePayment, "test_output_natwest_dates")
         # Check the output date format is correct
         with open(output_path) as csv_file:
             test_file_reader = csv.reader(csv_file)
             for row in test_file_reader:
                 self.assertRegex(row[18], "[0-9]{8}")
 
-    def test_line_endings(self):
-        for subcls in ProcessedPayment.__subclasses__():
-            if subcls != ProcessedPayment:
-                output_path = self.create_file(subcls, "test_output_line_ending_file")
-                with open(output_path) as csv_file:
-                    for line in csv_file:
-                        self.assertNotRegex(line, "^\n$")
-
-    def test_convert_file_extension(self):
-        for subcls in ProcessedPayment.__subclasses__():
-            if subcls != ProcessedPayment:
-                output_path = self.create_file(subcls, "test_output_line_ending_file")
-                self.assertEqual(output_path.suffix, ".txt")
-
     def test_natwest_standard_date(self):
-        output_path = self.create_file(NatwestBankLinePayment, "test_output_natwest_dates")
+        output_path = self.utils.create_file(NatwestBankLinePayment, "test_output_natwest_dates")
         with open(output_path) as csv_file:
             test_file_reader = csv.reader(csv_file)
             for row in test_file_reader:
@@ -67,11 +56,53 @@ class ConvertFileNoFrontend(unittest.TestCase):
                 self.assertEqual(parsed_date, datetime.date(datetime.today()) + timedelta(days=2))
 
     def test_natwest_long_number(self):
-        output_path = self.create_file(NatwestBankLinePayment, "test_output_natwest_long_number")
+        output_path = self.utils.create_file(NatwestBankLinePayment, "test_output_natwest_long_number")
         with open(output_path) as csv_file:
             test_file_reader = csv.reader(csv_file)
             for row in test_file_reader:
                 self.assertEqual(row[12], "56001412147931")
+
+
+class ConvertBarclaysNoFrontend(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.utils = ConverterTestUtils()
+
+    def tearDown(self):
+        files = glob.glob("{0}{1}{2}".format(str(Path("..", "..", "output").absolute()), os.sep, "test_output*"))
+        for f in files:
+            os.remove(f)
+
+    def test_convert_file_no_frontend_barclays(self):
+        output_path = self.utils.create_file(BarclaysPayment, "test_output_barclays")
+        self.assertTrue(os.path.isfile(str(output_path)))
+
+
+class ConvertAllFormatsNoFrontend(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.utils = ConverterTestUtils()
+
+    def tearDown(self):
+        files = glob.glob("{0}{1}{2}".format(str(Path("..", "..", "output").absolute()), os.sep, "test_output*"))
+        for f in files:
+            os.remove(f)
+
+    def test_line_endings(self):
+        for subcls in ProcessedPayment.__subclasses__():
+            if subcls != ProcessedPayment:
+                output_path = self.utils.create_file(subcls, "test_output_line_ending_file")
+                with open(output_path) as csv_file:
+                    for line in csv_file:
+                        self.assertNotRegex(line, "^\n$")
+
+    def test_convert_file_extension(self):
+        for subcls in ProcessedPayment.__subclasses__():
+            if subcls != ProcessedPayment:
+                output_path = self.utils.create_file(subcls, "test_output_line_ending_file")
+                self.assertEqual(output_path.suffix, ".txt")
 
 
 if __name__ == '__main__':
